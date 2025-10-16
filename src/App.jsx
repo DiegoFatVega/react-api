9615c5be34ff19b93c63674092779cd867beb4e6
DiegoFatVega <fatvegadiego@gmail.com>
2025-10-16 13:34:21 +0200

    Refactor header and improve actor image display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,17 @@ function App() {
 
   return (
     <>
-      <h1>CIAO</h1>
-      <header></header>
+      <header className='text-center bg-success p-5 h-100'>
+        <h1>Actors list (React-API)</h1>
+      </header>
       <main>
-        <div className='container-fluid'>
+        <div className='container-fluid mt-5'>
           <div className='row g-4'>
             {
               actorsList.map(actor =>
                 <div key={actor.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
                   <div className='card h-100'>
-                    <img src={actor.image} alt={actor.name} className="card-img-top p-3" style={{ height: "200px", objectFit: "contain" }} />
+                    <img src={actor.image} alt={actor.name} className="card-img-top p-3" style={{ height: "500px", objectFit: "contain" }} />
                     <div className="card-body text-center">
                       <h5 className="cart-title"> {actor.name}</h5>
                     </div>
